982141505aab7ed849f88d5b6b19f8d010ec86ed
hufei <hufei@geblab.com>
2019-01-25 10:55:16 +0800

    upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DotNetNative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Win32DlgTest.Test();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduce
+
+There are some unexpected behavior when use system timer in corert, but it is ok in dotnet core. Here is the demo project.
+
+Run publish.bat to build project in corert/native.
+
+Run "dotnet run" to run program in dotnet core.
+
+There are some unexpected behavior when comment "timer.Start()" of file "Win32DlgTest.cs" in corert.
